ca0cf42d2431aab1d1869f739abb8ff901cdfa8b
ajanthalakmal <lakmalajantha@gmail.com>
2022-06-22 12:51:14 +0530

    updated the details

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "lakmalajantha@gmail.com";
+$sitename = "Ajantha Lakmal CV";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
